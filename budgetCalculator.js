6e21cf75e5ee27012f01d80d0a44bf27308c9897
AyahAbdalla <115049581+AyahAbdalla@users.noreply.github.com>
2025-04-01 00:19:29 -0400

    Almost done, debt collector on vacation

--- budgetCalculator.js
+++ budgetCalculator.js
@@ -1,14 +1,33 @@
 let calcBtn = document.getElementById('calculateBudget')
-calcBtn.addEventListener('click', function(){
-    for (let i = 0; i<12;i++){
-        let userIncome = Number(prompt("please enter your total monthly income"));
-        let userExp = Number(prompt("Please enter you total monthly expenses"));
-        let userSav = userIncome-userExp;
-        if (userSav<=0){
-            window.alert("Warning: Spending exceeds income!")
+let result = document.getElementById('budgetResults')
+try {
+    calcBtn.addEventListener('click', function(){
+        for (let i = 0; i<3;i++){
+            let newP = document.createElement('p');
+            let userIncome = Number(prompt("please enter your total monthly income"));
+            let userExp = Number(prompt("Please enter you total monthly expenses"));
+            if (isNaN(userIncome) || isNaN(userExp)){
+                window.alert("your income or spending is not number, please reload to try again")
+                break 
+            }
+            let userSav = userIncome-userExp;
+            if (userSav<=0){
+                newP.textContent = "Warning! you are spending too much and are about to be in debt"
+                result.appendChild(newP)
+            }
+            newP.textContent = `Monthly Income: ${userIncome.toFixed(2)}`;
+            result.appendChild(newP);
+    
+            newP = document.createElement('p');
+            newP.textContent = `Monthly Expenses: ${userExp.toFixed(2)}`;
+            result.appendChild(newP);
+    
+            newP = document.createElement('p');
+            newP.textContent = `Monthly Savings: ${userSav.toFixed(2)}`;
+            result.appendChild(newP);    
         }
-        document.getElementById("budgetResults").textContent = '$' + userExp.toFixed(2); 
-        document.getElementById("budgetResults").textContent = '$' + userIncome.toFixed(2);
-        document.getElementById("budgetResults").textContent = '$' + userSav.toFixed(2);
-    }
-})
+    })
+
+} catch (error) {
+
+} 
